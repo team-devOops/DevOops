a0425b69549a34b7df96cc4aeec7867f20e37443
kbh0581 <kbh0581@gmail.com>
2019-11-01 23:57:15 +0900

    first Commit

---
+++ src/main/java/com/devoops/DevoopsApplication.java
@@ -0,0 +1,13 @@
+package com.devoops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevoopsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevoopsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/devoops/DevoopsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devoops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevoopsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
