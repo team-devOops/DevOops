fc41a71b05ce4d71613917124cee980c38d6d9b4
kbh0581 <kbh0581@gmail.com>
2020-01-19 19:26:36 +0900

    Modify  Board 생성자 주입으로 변경

--- src/main/java/com/devoops/controller/BoradController.java
+++ src/main/java/com/devoops/controller/BoradController.java
@@ -3,28 +3,27 @@ package com.devoops.controller;
 
 import com.devoops.domain.Board;
 import com.devoops.service.board.BoardService;
-import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 import java.util.Map;
 
 @RestController
 @RequestMapping("/board")
-
+@RequiredArgsConstructor
 public class BoradController {
 
-
-    private BoardService boardService;
-
+    private final BoardService boardService;
 
     @GetMapping("/list/{category}")
     public List<Board> list (@PathVariable Map<String,Object> pathVal){
-    	return boardService.listBoard((Integer)pathVal.get("category"));
+        return boardService.listBoard(Integer.parseInt(pathVal.get("category").toString()));
     }
+
 
 
 
--- src/main/java/com/devoops/service/board/BoardServiceImpl.java
+++ src/main/java/com/devoops/service/board/BoardServiceImpl.java
@@ -3,22 +3,16 @@ package com.devoops.service.board;
 
 import com.devoops.domain.Board;
 import com.devoops.repository.BoardRepository;
-import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
-
+@RequiredArgsConstructor
 public class BoardServiceImpl implements BoardService {
 
-    BoardServiceImpl(BoardRepository boardRepositor){
-        this.boardRepository = boardRepository;
-    }
-    
-    BoardRepository boardRepository;
+    private  final BoardRepository boardRepository;
 
     @Override
     public List<Board> listBoard(int category) {
