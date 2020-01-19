11ede4cc1a185372e3bd2a42d04a20abcf6dccef
zinzoddari <zinzoddari@gmail.com>
2020-01-19 15:44:21 +0900

    FIX postSignUp()

--- src/main/java/com/devoops/controller/auth/SignUpController.java
+++ src/main/java/com/devoops/controller/auth/SignUpController.java
@@ -7,22 +7,36 @@
 * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
 package com.devoops.controller.auth;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.devoops.component.BaseComponent;
 import com.devoops.domain.CustUser;
+import com.devoops.service.custuser.CustUserService;
 
 import lombok.extern.slf4j.Slf4j;
+import utils.CryptUtils;
+import utils.DateUtils;
 
 @RestController
 @Slf4j
-public class SignUpController {
+public class SignUpController extends BaseComponent {
 	
-
+	@Autowired
+	CustUserService custUserService;
 	
 	@PostMapping(value = "/auth/signUp")
 	public CustUser postSignUp(@RequestBody CustUser custUser) {
+		
+		custUser.setUserPw(CryptUtils.hashSHA256(custUser.getUserPw()));
+		//custUser.setEmailAuthYn("N");
+		custUser.setUseYn("Y");
+		custUser.setRegUserId(custUser.getUserId());
+		custUser.setRegDt(DateUtils.getTimeStamp());
+
+		custUserService.userSave(custUser);
 		
 		if(log.isDebugEnabled()) {
 			log.debug("custUser : " + custUser);
@@ -30,4 +44,5 @@ public class SignUpController {
 		
 		return custUser;
 	}
+	
 }
