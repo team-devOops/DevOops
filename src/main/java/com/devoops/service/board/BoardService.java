8d4e633d3c45d746b67bde1954e72e246ed5c20b
kbh0581 <kbh0581@gmail.com>
2020-01-27 16:11:21 +0900

    Merge : Merge Source

--- src/main/java/com/devoops/service/board/BoardService.java
+++ src/main/java/com/devoops/service/board/BoardService.java
@@ -1,12 +1,11 @@
 package com.devoops.service.board;
 
 import com.devoops.domain.Board;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
 public interface BoardService {
 
-    public List<Board> listBoard(int category, Pageable pageable);
+    List<Board> listBoard(int category, Pageable pageable);
 }
