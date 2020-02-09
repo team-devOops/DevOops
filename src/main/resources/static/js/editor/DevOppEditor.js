d3bb817548f6226823a8fa46470dcf3c82fbf34e
kbh0581 <kbh0581@gmail.com>
2020-02-10 00:34:41 +0900

    ADD : 마크다운 에디터 리펙토링

---
+++ src/main/resources/static/js/editor/DevOppEditor.js
@@ -0,0 +1,91 @@
+const DevOppEditor  = class Editor {
+
+    bindDom;
+    static _TAB_KEY_CODE = 9;
+    static _SPACE_KEY = '';
+
+    constructor(name,bindDom){
+        this.name = name;
+        this.bindDom = bindDom;
+        this.baseEdiorControll(this.bindDom);
+
+
+    }
+
+    get bindDome(){
+        return this.bindDom;
+    }
+
+    baseEdiorControll(dom) {
+        dom.addEventListener('keydown',(e) => this.keydown(e));
+        dom.addEventListener('keypress',(e) => this.keypress(e));
+    }
+
+    keydown(e) {
+        let code = e.which || e.keyCode || e.key;
+
+        switch (code) {
+            //Tab Key 구
+            case Editor._TAB_KEY_CODE :
+                e.preventDefault();
+
+                if (!window.getSelection) return;
+                const sel = window.getSelection();
+                if (!sel.rangeCount) return;
+                const range = sel.getRangeAt(0);
+                range.collapse(true);
+                const span = document.createElement('span');
+                span.appendChild(document.createTextNode('\t'));
+                span.style.whiteSpace = 'pre';
+
+                range.insertNode(span);
+                range.setStartAfter(span);
+                range.collapse(true);
+                sel.removeAllRanges();
+                sel.addRange(range);
+
+                break;
+        }
+
+    }
+
+    keypress(e){
+        let code = e.which || e.keyCode || e.key;
+        switch (code) {
+            //스페이스 구
+            case Editor._SPACE_KEY :
+                e.preventDefault();
+
+                if (!window.getSelection) return;
+                const sel = window.getSelection();
+                if (!sel.rangeCount) return;
+                const range = sel.getRangeAt(0);
+                range.collapse(true);
+                const span = document.createElement('span');
+                span.appendChild(document.createTextNode('\s'));
+                span.style.whiteSpace = 'pre';
+
+                range.insertNode(span);
+                range.setStartAfter(span);
+                range.collapse(true);
+                sel.removeAllRanges();
+                sel.addRange(range);
+
+                break;
+        }
+
+    }
+    bindDomEventAdd (eventName,action) {
+        this.bindDom.addEventListener(eventName,action);
+
+    }
+
+
+
+
+
+}
+
+
+
+
