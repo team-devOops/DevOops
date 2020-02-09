dfbbb6d71e122859cde63310f8eeced53daacd67
zinzoddari <zinzoddari@gmail.com>
2020-02-09 20:07:37 +0900

    ADD Interceptor Config

---
+++ src/main/java/com/devoops/config/WebMvcConfig.java
@@ -0,0 +1,27 @@
+package com.devoops.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+	@Autowired
+	@Qualifier(value = "loginInterceptor")
+	private HandlerInterceptor interceptor;
+
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(interceptor)
+				.excludePathPatterns("/**/*.css")
+				.excludePathPatterns("/**/*.js")
+				.excludePathPatterns("/**/*.map")
+				.excludePathPatterns("/error")
+				.excludePathPatterns("/**/*.ico");
+	}
+}
